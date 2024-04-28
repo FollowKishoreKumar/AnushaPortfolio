b27091d653c935bf0f98779d17c622b4bf47bd7f
FollowKishoreKumar <followkishorekumar@gmail.com>
2024-04-28 18:30:52 +0530

    Rough changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,29 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Anusha';
+
+  ngOnInit(): void {
+
+    // When the user clicks anywhere outside of the modal, close it
+    // let modal = document.getElementById('ticketModal');
+    // window.onclick = function (event) {
+    //   if (event.target == modal) {
+    //     modal.style.display = "none";
+    //   }
+    // }
+  }
+
+  // Used to toggle the menu on small screens when clicking on the menu button
+  callMe() {
+    let x = document.getElementById("navDemo");
+    if (x?.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else {
+      if(x !== null)
+      x.className = x?.className.replace(" w3-show", "");
+    }
+  }
+
 }
